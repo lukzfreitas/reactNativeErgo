a7ff2be933b58bb2df606dc8229a45a2430491d1
Lucas Antunes de Freitas <lucas.antunes@sistemafiergs.com.br>
2019-10-11 14:54:19 -0300

    add button

--- src/screens/FormQuestion.tsx
+++ src/screens/FormQuestion.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, FlatList, DeviceEventEmitter, TouchableOpacity } from 'react-native';
+import { SafeAreaView, Text, FlatList, DeviceEventEmitter, TouchableOpacity, StyleSheet, View, Alert } from 'react-native';
 import { NavigationScreenProp, NavigationState, NavigationParams } from 'react-navigation';
 import { Question } from '../components';
 
@@ -8,25 +8,31 @@ interface Props {
 }
 
 interface State {
-    questions: any[];
+    questionsId: number[];
+    questionsIdSelected: number[];
     count: number;
-    eventEmitter: any;
+    eventEmitter: any;    
 }
+
+const questions: any[] = require('../assets/questions.json').data;
 
 export class FormQuestion extends Component<Props, State> {
 
     constructor(props: Props) {
-        super(props);
+        super(props);        
         this.state = {
-            questions: require('../assets/questions.json').data,
+            questionsId: [],
+            questionsIdSelected: [],
             count: 0,
-            eventEmitter: null
-        };
-    }
+            eventEmitter: null            
+        };        
+    }    
 
     componentDidMount() {
-        console.log('ok')
-        this.setState({ eventEmitter: DeviceEventEmitter.addListener('eventKey', this.questionSelected) });
+        let questionsId = questions.map((question: any) => {
+            return question.id;
+        })
+        this.setState({ eventEmitter: DeviceEventEmitter.addListener('eventKey', this.questionSelected), questionsId: questionsId });
     }
 
     componentWillUnmount() {
@@ -34,22 +40,72 @@ export class FormQuestion extends Component<Props, State> {
     }
 
     questionSelected = (question: any) => {
-        console.log(question);
+        if (this.state.questionsIdSelected.indexOf(question.id) > -1) return;
+        this.state.questionsIdSelected.push(question.id);
+        console.log(this.state.questionsIdSelected);
+    }
+
+    saveQuestions = () => {        
+        let questionsNotSelected = this.state.questionsId.filter((i) => {
+            return this.state.questionsIdSelected.indexOf(i) < 0
+        });
+
+        
+        Alert.alert(
+            'Questões abaixo não foram respondidas',
+            'teste1' + '\n' + 'teste2'
+        )
     }
 
 
-    render() {
-        const { questions } = this.state;
+    render() {        
         return (
-            <View>
-                <FlatList
-                    data={questions}
-                    keyExtractor={question => question.description}
-                    renderItem={({ item, index, separators }) => (
-                        <Question item={item} />
-                    )}
-                />
+            <View style={style.screen}>
+                <View style={style.contentList}>
+                    <SafeAreaView>
+                        <FlatList
+                            data={questions}
+                            keyExtractor={(question: any) => question.description}
+                            renderItem={({ item, index, separators }) => (
+                                <Question item={item} />
+                            )}
+                        />
+                    </SafeAreaView>
+                </View>
+                <View style={style.footer}>
+                    <TouchableOpacity style={style.button} onPress={this.saveQuestions} >
+                        <Text style={style.textButton}>Salvar Questionário</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     }
 }
+
+const style = StyleSheet.create({
+    screen: {
+        flex: 10,
+        flexDirection: 'column'
+    },
+    contentList: {
+        flex: 9
+    },
+    footer: {        
+        backgroundColor: 'transparent',        
+        shadowOpacity: 0.8,        
+        borderColor: 'black',        
+        elevation: 50
+    },    
+    button: {
+        shadowOpacity: 0.9,
+        alignSelf: 'center',
+        margin: 15
+    },
+    textButton: {
+        alignSelf: 'center',
+        fontSize: 20,
+        textAlign: 'center',
+        color: 'green',
+        fontWeight: 'bold'
+    }
+})
